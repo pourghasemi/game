e5db18a5e7e7994d9dc3bb35b868e49511855132
shiva.pourghasemi <shiva.pourghasemi@yahoo.com>
2025-01-20 14:45:08 +0100

    fix style

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -46,7 +46,7 @@
             />
             <button
               @click="actionsObj[userAction]"
-              class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              class="w-full text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
               {{ userAction === 'login' ? 'Sign in' : 'Sign up' }}
             </button>
